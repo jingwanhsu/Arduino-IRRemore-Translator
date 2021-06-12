f9060ce7d01debf0628226329f9073399c9fc7db
jingwan <hsugaigai@gmail.com>
2021-06-12 16:26:04 -0700

    Update header comments

--- remote_click/IRRemoteTranslator.h
+++ remote_click/IRRemoteTranslator.h
@@ -1,48 +1,14 @@
 /*
- * ReceiveAndSend.cpp
+ * IRRemoteTranslator.h
  *
- * Record and play back last received IR signal at button press.
+ * Translater incoming IR signal to a set of outgoing IR signals
  * The logic is:
- * If the button is pressed, send the IR code.
- * If an IR code is received, record it.
+ * Define the original signal you want to translate
+ * Define the set of translated signals with delay in between
  *
- * An example for simultaneous receiving and sending is in the UnitTest example.
+ * An example is remote_click.ino which translate the signals of
+ * CD player remote to of TV remote.
  *
- * An IR detector/demodulator must be connected to the input IR_RECEIVE_PIN.
- *
- * A button must be connected between the input SEND_BUTTON_PIN and ground.
- * A visible LED can be connected to STATUS_PIN to provide status.
- *
- *
- * Initially coded 2009 Ken Shirriff http://www.righto.com
- *
- *  This file is part of Arduino-IRremote
- *https://github.com/Arduino-IRremote/Arduino-IRremote.
- *
- ************************************************************************************
- * MIT License
- *
- * Copyright (c) 2009-2021 Ken Shirriff, Armin Joachimsmeyer
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- *furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- *all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *SOFTWARE.
- *
- ************************************************************************************
  */
 #include <Arduino.h>
 #include <IRremote.h>
